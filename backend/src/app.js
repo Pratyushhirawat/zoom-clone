d9a2433942c39cf1bd51d39928bd60d1f58fe4fc
Pratyush Hirawat <154426448+Pratyushhirawat@users.noreply.github.com>
2026-06-17 12:45:45 +0530

    completed project

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+import express from'express';
+import {createServer} from 'node:http';
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+import 'dotenv/config';
+import { connectToSocket } from './controllers/socketManager.js'; 
+import userRoutes from "./routes/users.routes.js";
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    const connectionDb = await mongoose.connect(process.env.MONGODB_URI)
+    console.log(`MongoDB Connected Host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("Listining on port 8000")
+    });
+
+}
+
+start()
